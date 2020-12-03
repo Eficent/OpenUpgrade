061fe60ba85ee16710766c9fd1c940f1e70449db
mreficent <miquel.raich@eficent.com>
2020-12-03 15:24:39 +0100

    [12.0][UPD] apriori: avoid renaming account_asset

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -54,14 +54,14 @@ merged_modules = {
     # OCA/account-analytic
     # although model is defined in "analytic", logic is in "account"
     'account_analytic_distribution': 'account',
-    'account_asset_analytic': 'account_asset_management',
+    # 'account_asset_analytic': 'account_asset_management',
     # OCA/account-financial-reporting
     'customer_activity_statement': 'partner_statement',
     'customer_outstanding_statement': 'partner_statement',
     # OCA/account-financial-tools
-    'account_asset': 'account_asset_management',
-    'account_asset_depr_line_cancel': 'account_asset_management',
-    'account_asset_disposal': 'account_asset_management',
+    # 'account_asset': 'account_asset_management',
+    # 'account_asset_depr_line_cancel': 'account_asset_management',
+    # 'account_asset_disposal': 'account_asset_management',
     'account_reversal': 'account',
     # OCA/e-commerce
     'website_sale_default_country': 'website_sale',
@@ -109,9 +109,9 @@ renamed_models = {
     'sale.quote.template': 'sale.order.template',
     'stock.incoterms': 'account.incoterms',
     # OCA/account-financial-tools
-    'account.asset.asset': 'account.asset',
-    'account.asset.depreciation.line': 'account.asset.line',
-    'account.asset.category': 'account.asset.profile',
+    # 'account.asset.asset': 'account.asset',
+    # 'account.asset.depreciation.line': 'account.asset.line',
+    # 'account.asset.category': 'account.asset.profile',
 }
 
 # only used here for openupgrade_records analysis:
